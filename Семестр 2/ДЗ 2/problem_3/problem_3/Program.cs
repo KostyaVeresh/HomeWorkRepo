712cbf6cd4aad8fbd73f839a364aa4f76b6ebb9b
KostyaVeresh <kostya.veresh@mail.ru>
2013-02-24 21:17:30 +0400

    ф-ция FillArray: заполнение и вывод матрицы length1*length2.

---
+++ Семестр 2/ДЗ 2/problem_3/problem_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace problem_3
+{
+    class Program
+    {
+        private static void FillArray(int[,] mas, int length1, int length2)
+        {
+            int num = 10;
+            for (int i = 0; i < length1; ++i)
+            {
+                for (int j = 0; j < length2; ++j)
+                {
+                    mas[i, j] = num;
+                    Console.Write("{0} ", mas[i, j]);
+                    ++num;
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int length1 = 7;
+            int length2 = 7;
+            int[,] mas = new int[length1, length2];
+            FillArray(mas, length1, length2);
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
