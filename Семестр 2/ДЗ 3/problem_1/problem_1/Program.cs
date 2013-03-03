5a32e0c6bb50b1a848311d29e6025f21f9512bba
KostyaVeresh <kostya.veresh@mail.ru>
2013-03-03 20:50:53 +0400

    Задача 1 ДЗ 3: реализация стека в виде класса

---
+++ Семестр 2/ДЗ 3/problem_1/problem_1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace problem_1
+{
+    class StackElement
+    {
+        public int value;
+        public StackElement next;
+    }
+
+    class Stack
+    {
+        public StackElement head;
+
+        public Stack()
+        {
+            head = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public void Push(int num)
+        {
+            StackElement temp = new StackElement();
+            temp.value = num;
+            temp.next = head;
+            head = temp;
+        }
+
+        public int Top()
+        {
+            return head.value;
+        }
+
+        public int Pop()
+        {
+            int result = Top();
+            StackElement tmp = head;
+            head = head.next;
+            return result;
+        }
+
+        public void printStack()
+        {
+            StackElement tmp = head;
+            while (tmp != null)
+            {
+                Console.Write("{0} ", tmp.value);
+                tmp = tmp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stack = new Stack();
+            for (int i = 1; i <= 5; ++i)
+            {
+                stack.Push(i);
+            }
+            stack.printStack();
+            Console.WriteLine("The top of stack: {0}", stack.Top());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Семестр 2/ДЗ 3/problem_1/problem_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("problem_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("problem_1")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("36d359a5-a736-40f2-aa56-b4dd8e815396")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
