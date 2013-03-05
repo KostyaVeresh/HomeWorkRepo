eb66e242949e7c27d3f0deb95f63159dec471e06
KostyaVeresh <kostya.veresh@mail.ru>
2013-03-06 01:22:03 +0400

    Класс Stack в отдельном файле, исправлена видимость полей.

--- Семестр 2/ДЗ 3/problem_1/problem_1/Program.cs
+++ Семестр 2/ДЗ 3/problem_1/problem_1/Program.cs
@@ -1,60 +1,7 @@
 ﻿using System;
 
 namespace problem_1
-{
-    class StackElement
-    {
-        public int value;
-        public StackElement next;
-    }
-
-    class Stack
-    {
-        public StackElement head;
-
-        public Stack()
-        {
-            head = null;
-        }
-
-        public bool IsEmpty()
-        {
-            return head == null;
-        }
-
-        public void Push(int num)
-        {
-            StackElement temp = new StackElement();
-            temp.value = num;
-            temp.next = head;
-            head = temp;
-        }
-
-        public int Top()
-        {
-            return head.value;
-        }
-
-        public int Pop()
-        {
-            int result = Top();
-            StackElement tmp = head;
-            head = head.next;
-            return result;
-        }
-
-        public void printStack()
-        {
-            StackElement tmp = head;
-            while (tmp != null)
-            {
-                Console.Write("{0} ", tmp.value);
-                tmp = tmp.next;
-            }
-            Console.WriteLine();
-        }
-    }
-
+{   
     class Program
     {
         static void Main(string[] args)
---
+++ Семестр 2/ДЗ 3/problem_1/problem_1/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace problem_1
+{
+    class Stack
+    {
+        private class StackElement
+        {
+            public int value;
+            public StackElement next;
+        }
+        private StackElement head;
+
+        public Stack()
+        {
+            head = null;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public void Push(int num)
+        {
+            StackElement temp = new StackElement();
+            temp.value = num;
+            temp.next = head;
+            head = temp;
+        }
+
+        public int Top()
+        {
+            return head.value;
+        }
+
+        public int Pop()
+        {
+            int result = Top();
+            head = head.next;
+            return result;
+        }
+
+        public void printStack()
+        {
+            StackElement tmp = head;
+            while (tmp != null)
+            {
+                Console.Write("{0} ", tmp.value);
+                tmp = tmp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
