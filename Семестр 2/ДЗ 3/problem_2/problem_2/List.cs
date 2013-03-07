5f889a965cbbe821341432327d167866078fefe9
KostyaVeresh <kostya.veresh@mail.ru>
2013-03-07 22:00:50 +0400

    Не работает класс List

---
+++ Семестр 2/ДЗ 3/problem_2/problem_2/List.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace problem_2
+{
+    class List
+    {
+        private class ListElement
+        {
+            public int value;
+            public ListElement next;
+        }
+        private ListElement head;
+
+        public List()
+        {
+            head.next = null;
+            head.value = 0;
+        }
+
+        public ListElement elemPos(int value)
+        {
+            ListElement tmp = head;
+            while (tmp.value != value)
+                tmp = tmp.next;
+            return tmp;
+        }
+
+        public void addElem(ListElement pos, int value)
+        {
+	        ListElement tmp = new ListElement();
+	        tmp.value = value;
+	        tmp.next = pos.next;
+	        pos.next = tmp;
+        }
+
+        public void deleteElem(int value)
+        {
+            ListElement tmp = head;
+            while (tmp.next.value != value)
+                tmp = tmp.next;
+            ListElement tmp1 = elemPos(value);
+            tmp.next = tmp1.next;  
+        }
+
+        public void printList()
+        {
+            ListElement tmp = head;
+            while (tmp.next != null)
+            {
+                Console.Write("{0} ", tmp.next.value);
+                tmp = tmp.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
