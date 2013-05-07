b482e205257699bf3f64507130823e876ab6d9ff
KostyaVeresh <kostya.veresh@mail.ru>
2013-05-07 15:29:42 +0400

    Переписывание к/р

---
+++ Семестр 2/КР_1_переписывание/problem/problem/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace problem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            //Moves the button by entering the mouse
+            if (MousePosition.X - Left > 100)
+                button1.Left -= 75;
+            else
+                button1.Left += 75;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Close the form by the mouse click.
+            Close();
+        }
+    }
+}
