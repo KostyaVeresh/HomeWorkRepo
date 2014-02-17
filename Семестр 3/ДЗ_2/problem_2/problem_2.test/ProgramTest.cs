1364064a17c9e1a2863bace5caf5e311f38f5446
KostyaVeresh <kostya.veresh@mail.ru>
2014-02-17 20:28:20 +0400

    added "problem 2" from h/w 2

---
+++ Семестр 3/ДЗ_2/problem_2/problem_2.test/ProgramTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace problem_2.test
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void ZeroElementsTest()
+        {
+            int[] arr = { 2, 0, 5, 3, 2, 0 };
+            Assert.AreEqual(Program.zeroElements(arr), 2);
+            arr[2] = 0;
+            Assert.AreEqual(Program.zeroElements(arr), 3);
+        }
+    }
+}
---
+++ Семестр 3/ДЗ_2/problem_2/problem_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_2
+{
+    public class Program
+    {
+        public static int zeroElements(int[] array)
+        {
+            int count = 0;
+            for (int i = 0; i < array.Length; ++i)
+            {
+                if (array[i] == 0)
+                    ++count;
+            }
+            return count;
+        }
+
+        static void Main(string[] args)
+        {
+        }
+    }
+}
