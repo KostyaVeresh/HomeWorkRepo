936b133cdb6cf81132f4a7d2beba47f7f5a5b987
KostyaVeresh <kostya.veresh@mail.ru>
2014-02-17 22:57:55 +0400

    added problem 1 h/w 2

---
+++ Семестр 3/ДЗ_2/problem_1/problem_1.test/RobotsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace problem_1.test
+{
+    [TestClass]
+    public class RobotsTest
+    {
+        [TestMethod]
+        public void RobotsGraphTest()
+        {
+            int[,] adMatrix = { {0, 1, 0, 0}
+                              , {1, 0, 1, 0}
+                              , {0, 1, 0, 1}
+                              , {0, 0, 1, 0}
+                              };
+            int[] robots = { 0, 3 };
+            RobotsGraph gr = new RobotsGraph(adMatrix, robots);
+            Assert.IsFalse(gr.Result);
+        }
+    }
+}
---
+++ Семестр 3/ДЗ_2/problem_1/problem_1/RobotsGraph.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problem_1
+{
+    public class RobotsGraph
+    {
+        private int[,] adjacencyMatrix;
+        private bool[] visitedVertices;
+        private int[,] newMatrix;
+        public bool Result { get; private set; }
+
+        /// <summary>
+        /// Creates matrix of robots' way and gives a result if they are exploded
+        /// </summary>
+        /// <param name="adMatrix"></param>
+        /// <param name="robotCoordinates"></param>
+        public RobotsGraph(int[,] adMatrix, int[] robotCoordinates)
+        {
+            this.adjacencyMatrix = adMatrix;
+            this.newMatrix = new int[adjacencyMatrix.GetLength(0),adjacencyMatrix.GetLength(0)];
+            this.visitedVertices = new bool[adjacencyMatrix.GetLength(0)];
+            for (int i = 0; i < adjacencyMatrix.GetLength(0); ++i)
+                visitedVertices[i] = false;
+            CreateNewMatrix(0);
+            bool tmp = false;
+
+            for (int i = 0; i < robotCoordinates.Length; ++i)
+            {
+                for (int j = 0; j < robotCoordinates.Length; ++j)
+                {
+                    if (WayExists(robotCoordinates[i], robotCoordinates[j]))
+                        tmp = true;
+                }
+                if (!tmp)
+                {
+                    Result = false;
+                    return;
+                }
+                tmp = false;
+            }
+        }
+
+        /// <summary>
+        /// Creates matrix of "over-one" connection
+        /// </summary>
+        /// <param name="index"></param>
+        private void CreateNewMatrix(int index)
+        {
+            visitedVertices[index] = true;
+            for (int i = 0; i < adjacencyMatrix.GetLength(0); ++i)
+                if ((adjacencyMatrix[index, i] != 0) && (!visitedVertices[i]))
+                {
+                    visitedVertices[i] = true;
+                    for (int j = 0; j < adjacencyMatrix.GetLength(0); ++j)
+                    {
+                        if ((adjacencyMatrix[i, j] != 0) && (!visitedVertices[j]))
+                        {
+                            newMatrix[index, j] = 1;
+                            newMatrix[j, index] = 1;
+                            CreateNewMatrix(j);
+                        }
+                    }
+                }
+            for (int i = 0; i < adjacencyMatrix.GetLength(0); ++i)
+                visitedVertices[i] = false;
+        }
+
+        private bool WayExists(int first, int second)
+        {
+            if (newMatrix[first, second] == 1)
+                return true;
+            visitedVertices[first] = true;
+            for (int i = 0; i < adjacencyMatrix.GetLength(0); ++i)
+                if ((newMatrix[first, i] != 0) && (!visitedVertices[i]))
+                {
+                    if (WayExists(i, second))
+                        return true;
+                }
+            return false;
+        }
+    }
+}
