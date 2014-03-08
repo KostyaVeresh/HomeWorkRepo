453a22e7bbe084b985ff1f24b3eefc26a85e2f66
KostyaVeresh <kostya.veresh@mail.ru>
2014-03-09 01:22:59 +0400

    added 2 problems from HW_2

---
+++ Семестр 4/ДЗ_2/problem2/problem2/Program.fs
@@ -0,0 +1,16 @@
+﻿open System
+
+let listPowerTwo () =
+    let rec makeList n =
+        match n with
+        | 0 -> []
+        | _ -> makeList(n - 1) @ [pown 2 n]
+    makeList 5 
+
+        
+[<EntryPoint>]
+let main argv = 
+    let list = listPowerTwo()
+    Console.WriteLine(list);
+    Console.ReadLine();
+    0
---
+++ Семестр 4/ДЗ_2/problem_1/problem_1/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+//Returns reversed list
+let rec reverse list =
+    match list with
+    | [] -> []
+    | h::t -> reverse(list.Tail) @ [h]
+
+[<EntryPoint>]
+let main argv =
+    let list = [1..10]
+    let newList = reverse list
+    Console.WriteLine(newList)
+    Console.ReadLine();
+    0
