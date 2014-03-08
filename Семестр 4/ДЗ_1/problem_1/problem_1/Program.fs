fc894c8ba96084354568c2d07ed1b4839b1b627e
KostyaVeresh <kostya.veresh@mail.ru>
2014-03-08 21:31:23 +0400

    added homework 1

---
+++ Семестр 4/ДЗ_1/problem_1/problem_1/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+//Counts factorial of natural number
+let rec fact n =
+    match n with
+    | 0 -> 1
+    | _ -> fact(n - 1) * n
+
+[<EntryPoint>]
+let main n = 
+    //let res = fact 5
+    //printfn "Test results for 5: %d" res
+    Console.WriteLine("Test results for 0,1,5,6: {0} {1} {2} {3}", fact 0, fact 1, fact 5, fact 6)
+    Console.ReadLine()
+    0
---
+++ Семестр 4/ДЗ_1/problem_2/problem_2/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+//Counts the nth Fibonacci number
+let rec fibonacci n =
+    match n with
+    | 0 -> 0
+    | 1 -> 1
+    | _ -> fibonacci(n - 1) + fibonacci(n - 2)
+
+[<EntryPoint>]
+let main n = 
+    Console.WriteLine("Test results for first 5 numbers: {0} {1} {2} {3} {4}", fibonacci 1, fibonacci 2, fibonacci 3, fibonacci 4, fibonacci 5);
+    Console.ReadLine();
+    0
