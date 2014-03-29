2315985e518da5affbfe5421445381544435046e
KostyaVeresh <kostya.veresh@mail.ru>
2014-03-29 21:32:22 +0400

    added 4.2

---
+++ Семестр 4/ДЗ_4/problem_2/problem_2/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let listMap: int -> int list -> int list =
+    List.map << (*)
+
+[<EntryPoint>]
+let main argv = 
+    let list = [1;2;3]
+    let res = listMap 2 list
+    printf "%A" res
+    ignore(Console.ReadLine())
+    0
