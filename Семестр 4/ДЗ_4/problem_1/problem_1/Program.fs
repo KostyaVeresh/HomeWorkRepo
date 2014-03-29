b5c9023e5dfda2aeb43fc3daf547d4aca37f47cc
KostyaVeresh <kostya.veresh@mail.ru>
2014-03-29 19:53:47 +0400

    added 4.1

---
+++ Семестр 4/ДЗ_4/problem_1/problem_1/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+let brackets (str: string) =
+    let list = []
+    let openBr char =
+        match char with
+        | ')' -> '('
+        | ']' -> '['
+        | '}' -> '{'
+        | _ -> ' '
+    let rec check (list : char list) num =
+        if num = str.Length then list = [] else
+        let elem = str.Chars(num)
+        match elem with
+        | '(' | '[' | '{' -> check ([elem] @ list) (num + 1)
+        | ')' | ']' | '}' ->
+            match list with
+            | [] -> false
+            | h :: t -> if h = openBr elem then check (list.Tail) (num + 1) else false
+        | _ -> check list (num + 1)
+    check [] 0
+
+[<EntryPoint>]
+let main argv = 
+    let str = "() ({} [] [{ () () }])"
+    let res = brackets str
+    if res then 
+        printfn "is correct" 
+    else 
+        printfn "is not correct"
+    ignore(Console.ReadLine())
+    0
