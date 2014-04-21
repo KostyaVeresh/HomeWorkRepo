f1f14dbe39bc917aace290bff8adcef6a7ca4e32
KostyaVeresh <kostya.veresh@mail.ru>
2014-04-21 19:07:14 +0400

    added 4.5

---
+++ Семестр 4/ДЗ_4/problem_5/problem_5/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+type BinTree = 
+    | Tree of int * BinTree * BinTree
+    | Leaf of int
+
+let rec mapTree (tree: BinTree) mapFunc =
+    match tree with
+    | Tree(value, left, right) -> Tree(mapFunc value, mapTree left mapFunc, mapTree right mapFunc)
+    | Leaf(value) -> Leaf(mapFunc value)
+
+[<EntryPoint>]
+let main argv = 
+    ignore(Console.ReadLine())
+    0
