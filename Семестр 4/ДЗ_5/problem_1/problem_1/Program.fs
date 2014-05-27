f868abf2c71bd4afb2798b95b101b7cde40bda60
KostyaVeresh <kostya.veresh@mail.ru>
2014-05-27 12:32:41 +0400

    added 7.1, 5.1

---
+++ Семестр 4/ДЗ_5/problem_1/problem_1/Program.fs
@@ -0,0 +1,57 @@
+﻿type OperSystem =
+    | Windows
+    | Linux
+    | Mac
+
+type Computer(os, virused) =
+    let mutable virused = virused
+    let syst = os
+    member this.IsVirused = virused
+    member this.OperSyst = syst
+    member this.Infect() =
+        let rand = new System.Random()
+        let value = rand.Next(0, 100)
+        match syst with
+        | Windows -> 
+            if value < 50 then virused <- true
+        | Linux -> 
+            if value < 50 then virused <- true
+        | Mac -> 
+            if value < 50 then virused <- true
+
+type Network(adjacencyMatrix : int[][], computerOS : OperSystem[], virusedComputers : bool[]) =
+    let adjacencyMatrix = adjacencyMatrix
+    let computersOfNet = [|for i in 1 .. computerOS.Length -> new Computer(computerOS.[i], virusedComputers.[i])|]
+    let TryToInfect =
+        let canBeVirused = [|for i in 1 .. computersOfNet.Length -> false|]
+        let rec func1 acc1 =
+            match acc1 with
+            | x when x = computersOfNet.Length + 1 -> ignore()
+            | _ -> 
+                if computersOfNet.[acc1].IsVirused then 
+                    let rec func2 acc2 =
+                        match acc2 with
+                        | x when x = computersOfNet.Length + 1 -> ignore()
+                        | _ -> 
+                            if adjacencyMatrix.[acc1].[acc2] = 1 then canBeVirused.[acc2] <- true
+                            func2 (acc2 + 1)
+                    func2 1
+                func1 (acc1 + 1)
+        func1 1
+        canBeVirused
+
+    member this.ComputersOfNet = computersOfNet
+    member this.NextState() =
+        let rec func acc =
+            match acc with
+            | x when x = computersOfNet.Length + 1 -> ignore()
+            | _ -> 
+                if TryToInfect.[acc] then computersOfNet.[acc].Infect()
+                func (acc + 1)
+        func 1
+
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0
---
+++ Семестр 4/ДЗ_7/problem_1/problem_1/Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open System.ComponentModel
+
+let array = [|for i in 1 .. 1000000 -> 1|]
+let sum =
+    let threads = [|for i in 1 .. 100 -> new BackgroundWorker()|]
+    let result = ref 0
+    let init = List.map (fun i -> 
+        threads.[i].DoWork.Add(fun args ->
+            let res = Array.sum (Array.sub array i 10000)
+            args.Result <- box res)
+        threads.[i].RunWorkerCompleted.Add(fun args ->
+            result := !result  + unbox args.Result)) [0 .. 99]
+    let mutable isThreadWorks = [|for i in 0 .. 99 -> threads.[i].IsBusy|]
+    let start = List.map (fun i -> threads.[i].RunWorkerAsync()) [0 .. 99]
+    isThreadWorks <- [| for i in 0 .. 99 -> threads.[i].IsBusy |]
+    while ((Array.tryFind (fun x -> x = true) isThreadWorks) <> None) do
+        isThreadWorks <- [| for i in 0 .. 99 -> threads.[i].IsBusy |]
+    result.Value
+
+printfn "%A" (sum)
+ignore <| Console.ReadLine()
+
