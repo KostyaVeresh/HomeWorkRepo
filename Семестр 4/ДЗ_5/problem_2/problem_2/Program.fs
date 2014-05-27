e97ff173728a0241e68296fb4fc394fbaea24bce
KostyaVeresh <kostya.veresh@mail.ru>
2014-05-27 19:15:56 +0400

    added 5.2 and 6.1

---
+++ Семестр 4/ДЗ_5/problem_2/problem_2/Program.fs
@@ -0,0 +1,37 @@
+﻿open System
+open System.Collections
+open System.Collections.Generic
+
+type TreeNode<'a> =
+    | TreeNode of 'a * TreeNode<'a> * TreeNode<'a>
+    | Empty
+
+type Iterator<'a>(tree : TreeNode<'a>) =
+    let rec toList = function
+    | Empty -> []
+    | TreeNode(elem, left, right) -> toList(left) @ elem :: toList(right)
+    let mutable list = toList tree
+    let mutable currentPos = -1
+    interface IEnumerator with
+        member this.MoveNext() =
+            if (currentPos + 1) < list.Length then
+                currentPos <- currentPos + 1
+                true
+            else false
+        member this.Current = box list.[currentPos]
+        member this.Reset() = currentPos <- -1
+
+type Tree() =
+    let mutable node = TreeNode.Empty
+    member this.Add(value) =
+        let rec insert value tree=
+            match tree with
+            | Empty -> TreeNode(value, Empty, Empty)
+            | TreeNode(v, left, right) when value < v -> 
+                TreeNode(v, insert value left, right)
+            | TreeNode(v, left, right) when value > v -> 
+                TreeNode(v, left, insert value right)
+            | TreeNode(_, _, _) as n -> n
+        node <- insert (value) (node)
+    interface IEnumerable with
+        member this.GetEnumerator() = new Iterator<'a>(node) :> IEnumerator
---
+++ Семестр 4/ДЗ_6/problem_1/problem_1/Program.fs
@@ -0,0 +1,14 @@
+﻿open System
+
+type RoundBuilder(num : int) =
+    member this.Bind (x : float, func : float -> float) = Math.Round(func x, num)
+    member this.Return (x : float) = Math.Round(x, num)
+
+let result = RoundBuilder 3 {
+    let! a = 2.0 / 12.0
+    let! b = 3.5
+    return a / b
+    }
+
+printf "%A" result
+ignore <| Console.ReadLine()
