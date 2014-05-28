e1572adeb30baea6ef92e934879779306336157c
KostyaVeresh <kostya.veresh@mail.ru>
2014-05-28 13:16:52 +0400

    fixed 4.3

--- Семестр 4/ДЗ_4/problem_3/problem_3/Program.fs
+++ Семестр 4/ДЗ_4/problem_3/problem_3/Program.fs
@@ -5,6 +5,20 @@ type Record = {Name : string; Number : int}
 
 let phoneDir () =
     let enter () = Console.ReadLine()
+    let enterNum () = int(enter())
+    let getName x = x.Name
+    let getNum x = x.Number
+
+    let find (enterStr : string) (getIdFunc) (get1) (list: Record list) (get2: Record -> 'b) (str: string)  =
+        Console.WriteLine(enterStr)
+        let id = getIdFunc()
+        match List.tryFind (fun x -> get1 x = id) list with
+        | Some x -> Console.WriteLine(str, get2(x));
+        | None -> printfn "Not found"
+
+    let enterCommand() = 
+        printfn "Enter command: 1 - to add new record, 2 - to find phone number, 3 - to find name
+        4 - to save records, 5- to read records"
     let rec command (list: Record list) = function
         | "0" -> ignore()
         | "1" -> 
@@ -12,30 +26,22 @@ let phoneDir () =
             let name = enter();
             printfn "Enter new phone number:"
             let number = enter() |> int;
-            printfn "Enter comand:"
-            command (list @ [{Name = name; Number = number}]) (enter())
+            enterCommand()
+            command ({Name = name; Number = number} :: list) (enter())
         | "2" -> 
-            printfn "Enter name:" 
-            let name = enter()
-            match List.tryFind (fun x -> x.Name = name) list with
-            | Some x -> printfn "Phone number: %d" x.Number
-            | None -> printfn "Not found"
-            printfn "Enter comand:"
+            find ("Enter name") enter getName list getNum ("Number: ") 
+            enterCommand()
             command list (enter())
         | "3" -> 
-            printfn "Enter number:" 
-            let num = enter() |> int
-            match List.tryFind (fun x -> x.Number = num) list with
-            | Some x -> printfn "Name: %s" x.Name
-            | None -> printfn "Not found"
-            printfn "Enter comand:"
+            find ("Enter phone") enterNum getNum list getName ("Name: ")
+            enterCommand()
             command list (enter())
         | "4" ->
             printfn "Records saved to file"
             use sr = new StreamWriter("new_file.txt")
             List.iter (fun x -> sr.WriteLine(x.Name + " " + string x.Number)) list
             sr.Close()
-            printfn "Enter comand:"
+            enterCommand()
             command list (enter())
         | "5" ->
             printfn "Records were read from file"
@@ -43,16 +49,16 @@ let phoneDir () =
             let rec readfile (readList : Record list) =
                 if not sr.EndOfStream then
                     let words = (sr.ReadLine()).Split [|' '|]
-                    readfile (readList @ [{Name = words.[0]; Number = words.[1] |> int}])  
+                    readfile ({Name = words.[0]; Number = words.[1] |> int} :: readList)  
                 else
                     sr.Close()
                     readList         
-            printfn "Enter comand:"     
+            enterCommand()    
             command (list @ (readfile [])) (enter())
         | _ ->
             printfn "wrong command"
             command list (enter())
-    printfn "Enter comand:"
+    
     command [] (enter())
 
 [<EntryPoint>]
